9badf142b27248556f65581179a9ec50266fc5fd
Your Name <your.email@example.com>
2024-12-24 21:42:39 +0800

    first commit

--- components.d.ts
+++ components.d.ts
@@ -50,6 +50,7 @@ declare module 'vue' {
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconExporter: typeof import('./src/components/IconExporter.vue')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     IEpHouse: typeof import('~icons/ep/house')['default']
